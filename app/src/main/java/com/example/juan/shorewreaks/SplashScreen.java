b75112406a4c93804771d91464e319cdc28b4090
Your Name <you@example.com>
2019-01-23 20:07:57 +0100

    Eliminar clase extra y añadir hide action bar en clases especificas

--- app/src/main/java/com/example/juan/shorewreaks/SplashScreen.java
+++ app/src/main/java/com/example/juan/shorewreaks/SplashScreen.java
@@ -23,6 +23,8 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        getSupportActionBar().hide();
+
         TextView myTitle = (TextView)findViewById(R.id.textView);
         TextView mySubtitle = (TextView)findViewById(R.id.textView2);
         ImageView myImage = (ImageView)findViewById(R.id.imageView);
