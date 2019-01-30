80359bf4be17415ccb55f2cba701c7f4ab979a81
Negu <negu_1@hotmail.com>
2019-01-30 02:45:35 +0100

    LoginActivity completo

--- app/src/main/java/com/example/juan/shorewreaks/LoginActivity.java
+++ app/src/main/java/com/example/juan/shorewreaks/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.juan.shorewreaks;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -14,39 +15,40 @@ public class LoginActivity extends AppCompatActivity {
     protected Button mLoginButton;
 
     protected ImageView mImageView;
-
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        mUsername = (TextInputLayout)findViewById(R.id.passwordTIL);
-//        mUsername.requestFocus();
+        getSupportActionBar().hide();
 
+        TextInputLayout password = (TextInputLayout)findViewById(R.id.passwordTIL);
+        password.requestFocus();
 
-        mLoginButton = (Button) findViewById(R.id.loginbutton);
-      /*  mLoginButton.setOnClickListener(new View.OnClickListener(){
+        TextInputLayout username = (TextInputLayout)findViewById(R.id.usernameTIL);
+        username.requestFocus();
+
+        mSignUpTextview = findViewById(R.id.signup_text);
+
+        mSignUpTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SingUp.class);
+                startActivity(intent);
+            }
+        });
+
+        mLoginButton = (Button)findViewById(R.id.loginbutton);
+        mLoginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                Intent intent = new Intent(LoginActivity.this, SplashScreen.class);
                 startActivity(intent);
 
 
             }
         });
-*/
-        /*mSignUpTextview = (TextView)findViewById(R.id.signup_text);
-        mSignUpTextview.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-                startActivity(intent);
-
-
-            }
-        });*/
 
     }
 }
