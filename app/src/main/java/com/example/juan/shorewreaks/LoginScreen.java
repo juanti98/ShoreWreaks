d017967e34ea508462036a13cb42bb2bf7436ea2
Negu <negu_1@hotmail.com>
2019-02-06 17:24:38 +0100

    sigup ok?

--- app/src/main/java/com/example/juan/shorewreaks/LoginScreen.java
+++ app/src/main/java/com/example/juan/shorewreaks/LoginScreen.java
@@ -1,5 +1,6 @@
 package com.example.juan.shorewreaks;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -11,16 +12,47 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginScreen extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private Button btnSignIn;
+    private EditText etEmail, etPassword;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
+        mAuth = FirebaseAuth.getInstance();
+        btnSignIn = findViewById(R.id.btnLogin);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+        context = this;
+
         getSupportActionBar().hide();
 
+
         TextView btnSingUp = (TextView)findViewById(R.id.signup_text);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = etEmail.getText().toString();
+                password = etPassword.getText().toString();
+
+                signIn(email, password);
+            }
+        });
 
         btnSingUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +61,37 @@ public class LoginScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginScreen.this, "Welcome Back!",
+                                    Toast.LENGTH_SHORT).show();
+
+                            Intent intent2 = new Intent(context, MainScreen.class);
+                            startActivity(intent2);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginScreen.this, "Email or Password is incorrect",
+                                    Toast.LENGTH_SHORT).show();
+                            etPassword.setText("");
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/juan/shorewreaks/SingUp.java
+++ app/src/main/java/com/example/juan/shorewreaks/SingUp.java
@@ -1,36 +1,64 @@
 package com.example.juan.shorewreaks;
 
+import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.concurrent.TimeUnit;
 
-public class SingUp extends AppCompatActivity {
-    protected Button mCancelButton;
-    protected Button mSingupButton;
-    protected ImageView singUpGoogle;
-    GoogleSignInClient mGoogleSignInClient;
+public class SingUp extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    private Button mCancelButton;
+    private Button mSingupButton;
+    private ImageView singUpGoogle;
 
-    GoogleApiClient mGoogleApliClient;
+    private static final int RC_SIGN_IN = 777;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private static final String TAG = "GoogleActivity";
+
+
+    private EditText etUsername, etEmail, etPassword;
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleApiClient mGoogleApliClient;
+    private FirebaseAuth mAuth;
+    private Context c;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,25 +69,52 @@ public class SingUp extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleApliClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
+        c = this;
 
         getSupportActionBar().hide();
 
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //  goMainScreen();
+                }
+            }
+        };
 
-        mCancelButton = (Button)findViewById(R.id.btnCancel);
-        mSingupButton = (Button)findViewById(R.id.btnSingUp);
+        mCancelButton = (Button) findViewById(R.id.btnCancel);
+        mSingupButton = (Button) findViewById(R.id.btnSingUp);
         singUpGoogle = (ImageView) findViewById(R.id.imgGoogle);
 
+// Google
         singUpGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.imgGoogle:
-                        signIn();
-                        break;
-                    // ...
-                }
+                // Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApliClient);
+                Intent intent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(intent, RC_SIGN_IN);
+            }
+        });
+        //email
+
+        mSingupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username, password, email;
+
+                username = etUsername.getText().toString();
+                password = etPassword.getText().toString();
+                email = etEmail.getText().toString();
+
+                createAccount(email, password);
             }
         });
 
@@ -75,26 +130,93 @@ public class SingUp extends AppCompatActivity {
 
     }
 
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-//      startActivityForResult(signInIntent, LoginScreen.class);
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            etUsername.setText("");
+                            etPassword.setText("");
+                            etEmail.setText("");
+                            Toast.makeText(c, "Usuario Creado",
+                                    Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(c, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            //GoogleSignIn.getSignedInAccountFromIntent(data);
+            GoogleSignInResult resultado = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(resultado);
+        }
+
+
+    }
+
+    private void handleSignInResult(GoogleSignInResult resultado) {
+        if (resultado.isSuccess()) {
+            //   goMainScreen();
+        } else {
+            Toast.makeText(this, "No se puede inciar sesión", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+  /*  private void goMainScreen() {
+        Intent intent = new Intent(this, SignIn.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }*/
 
     @Override
     protected void onStart() {
         super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
     }
 
-    private void updateUI(GoogleSignInAccount account) {
-        if (account != null){
-            Intent intent = new Intent(SingUp.this, LoginScreen.class);
-            startActivity(intent);
-        }
-        else{
-            Intent intent = new Intent(SingUp.this, SingUp.class);
-            startActivity(intent);
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (firebaseAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
         }
     }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount signInAccount) {
+        //...
+        AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //...
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
 }
