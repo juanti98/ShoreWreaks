385403a06601db99583ef8366d7627dbd218e124
ivanhernandez <21741151@live.uem.es>
2019-02-06 19:02:37 +0100

    Facebook log in done

--- app/src/main/java/com/example/juan/shorewreaks/SingUp.java
+++ app/src/main/java/com/example/juan/shorewreaks/SingUp.java
@@ -1,9 +1,13 @@
 package com.example.juan.shorewreaks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
 
 public class SingUp extends AppCompatActivity {
     protected Button mCancelButton;
@@ -27,5 +31,25 @@ public class SingUp extends AppCompatActivity {
             }
         });
 
+        if (AccessToken.getCurrentAccessToken() == null) {
+            goLoginScreen();
+
+
+        }
+
+
+
+
     }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, SingUp.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+    public void logout(View view){
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
+    }
+
 }
