074a10a154ef526d383bb52c3031b77f1ae7ac0c
Your Name <you@example.com>
2019-02-20 20:55:46 +0100

    Cambios basicos

--- app/src/main/java/com/shorewreaks/juan/shorewreaks/PerfilUser.java
+++ app/src/main/java/com/shorewreaks/juan/shorewreaks/PerfilUser.java
@@ -3,6 +3,8 @@ package com.shorewreaks.juan.shorewreaks;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -19,19 +21,31 @@ import android.widget.TextView;
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PerfilUser extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Context c;
-    private EditText tv_name_user;
+    private EditText et_name_user, et_nombre, et_apellido;
     private TextView tv_mail_user;
     private ImageView img_user;
     private TextView tvNombreUser, tvEmail;
-
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_user);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        String username = "juanti98";
+        Users nuevoUser = new Users(username,"juanti98@gmail.com","Juan","Torres");
+        mDatabase.child("users").child(username).setValue(nuevoUser);
 
         cambioVistaUser();
         cargarDatos();
@@ -40,15 +54,30 @@ public class PerfilUser extends AppCompatActivity implements NavigationView.OnNa
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         if (user != null) {
-            String name = user.getDisplayName();
+            //String name = user.getDisplayName();
             String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
+            //Uri photoUrl = user.getPhotoUrl();
             String uid = user.getUid();
-
-            tvNombreUser.setText(name);
-            tv_name_user.setText(name);
+            //setNombreApellido(name);
+            //tvNombreUser.setText(name);
+            //et_name_user.setText(name);
             tvEmail.setText(email);
-            tv_mail_user.setText(email);
+            //tv_mail_user.setText(email);
+
+            mDatabase.child("users").child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    et_name_user.setText(dataSnapshot.child("username").getValue().toString());
+                    et_nombre.setText(dataSnapshot.child("name").getValue().toString());
+                    et_apellido.setText(dataSnapshot.child("lastname").getValue().toString());
+                    tv_mail_user.setText(dataSnapshot.child("email").getValue().toString());
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
 
         } else {
             goLoginScreen();
@@ -71,9 +100,34 @@ public class PerfilUser extends AppCompatActivity implements NavigationView.OnNa
 
     }
 
+    private void setNombreApellido(String name) {
+        int count = 0, count2;
+        String nombre = "", apellido = "";
+        /*
+        while(count < name.length()){
+            if (!(name.substring(count, count + 1).equals(" "))){
+                nombre += name.substring(count, count + 1);
+            } else if(name.substring(count, count + 1).equals(" ")){
+                count2 = count + 1;
+                while(count2 < name.length()){
+                    apellido += name.substring(count2, count2 + 1);
+                    count2++;
+                }
+                count = name.length();
+            }
+            count++;
+        }
+        */
+        et_nombre.setText(nombre);
+        et_apellido.setText(apellido);
+
+    }
+
     private void cargarDatos() {
-        tv_name_user = findViewById(R.id.tv_name_user);
+        et_name_user = findViewById(R.id.et_name_user);
         tv_mail_user = findViewById(R.id.tv_mail_user);
+        et_nombre = findViewById(R.id.et_name);
+        et_apellido = findViewById(R.id.et_last_name);
     }
 
     private void cambioVistaUser() {
@@ -113,8 +167,6 @@ public class PerfilUser extends AppCompatActivity implements NavigationView.OnNa
             startActivity(intent);
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
